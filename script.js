23f36aa01f26e0262896e764bfc1c544581d60f7
edwin <edwinhendly17@gmail.com>
2023-12-10 15:57:46 +0700

    updated

--- script.js
+++ script.js
@@ -1,71 +1,105 @@
-const cursor = document.querySelector('#cursor');
+const cursor = document.querySelector("#cursor");
+const container = document.querySelector("#container");
 let hue = 0;
 const mouse = {
-    x: undefined,
-    move: false
-}
+	x: undefined,
+	move: false,
+};
 
 const currentPosition = {
-    x: 0
-}
+	x: 0,
+};
 
-addEventListener('mousemove', function(e){
-    handleCursor(e);
-    slide(e);
+addEventListener("resize", function (e) {
+	if (innerWidth <= 1250) {
+		container.style.translate = 0;
+	}
 });
 
-addEventListener('mousedown', function(e){
-    mouse.x = e.clientX;
-    mouse.move = true;
+addEventListener("mousemove", function (e) {
+	if (innerWidth > 1250) {
+		handleCursor(e);
+		slide(e);
+	}
 });
 
-addEventListener('mouseup', function(e){
-    mouse.move = false;
+addEventListener("mousedown", function (e) {
+	mouse.x = e.clientX;
+	mouse.move = true;
 });
 
-function handleCursor(e){
-    cursor.style.top = `${e.clientY}px`;
-    cursor.style.left = `${e.clientX}px`;
-    cursor.style.background = `hsl(${hue++}deg, 100%, 50%)`;
+addEventListener("mouseup", function (e) {
+	mouse.move = false;
+});
+
+function handleCursor(e) {
+	cursor.style.top = `${e.clientY}px`;
+	cursor.style.left = `${e.clientX}px`;
+	cursor.style.background = `hsl(${hue++}deg, 100%, 50%)`;
 }
 
-function slide(e){
-    if(!mouse.move) return;
-    const container = document.querySelector('#container');
-    const images = document.querySelectorAll('.image img');
-    const deltaX = e.clientX - mouse.x;
-    let xPercent = (deltaX / (innerWidth / 2) * -100);
-    console.log(xPercent);
-    currentPosition.x += xPercent;
-    currentPosition.x = Math.min(0, currentPosition.x);
-    currentPosition.x = Math.max(-100, currentPosition.x);
+function slide(e) {
+	if (!mouse.move) return;
+	const images = document.querySelectorAll(".image img");
+	const deltaX = e.clientX - mouse.x;
+	let xPercent = (deltaX / (innerWidth / 2)) * -100;
+	console.log(xPercent);
+	currentPosition.x += xPercent;
+	currentPosition.x = Math.min(0, currentPosition.x);
+	currentPosition.x = Math.max(-100, currentPosition.x);
 
-    container.animate({
-        translate: `${currentPosition.x}%`
-    }, {
-        fill: 'forwards',
-        duration: 500
-    });
+	container.animate(
+		{
+			translate: `${currentPosition.x}%`,
+		},
+		{
+			fill: "forwards",
+			duration: 500,
+		}
+	);
 
-    images.forEach(image => {
-        image.animate({
-            objectPosition: `${-currentPosition.x}%`
-        }, {
-            fill: 'forwards',
-            duration: 100
-        });
-    });
+	images.forEach((image) => {
+		image.animate(
+			{
+				objectPosition: `${-currentPosition.x}%`,
+			},
+			{
+				fill: "forwards",
+				duration: 100,
+			}
+		);
+	});
 
-    mouse.x = e.clientX;
+	mouse.x = e.clientX;
 }
 
-
-
-const { promises: fs } = require('fs');
-const dir = '/';
-
-const getNumFiles = async (dir) => {
-  const files = await fs.readdir(dir)
-  console.log(files.length)
+function generateEmoticons(num = 100){
+    const emots = ['😂', '🤗', '🌟', '❤️', '😊', '😍', '🥳', '😁', '😎', '😏', '🫠', '😳', '😌', '🥰', '🦖', '🦕', '🐼', '💕', '😑', '🤤'];
+    for(let i = 0; i < num; i++){
+        const bubble = document.createElement('div');
+        bubble.classList.add('bubble');
+        bubble.innerHTML = emots[~~(Math.random() * emots.length)];
+        bubble.style.animationDelay = Math.random() * 10;
+        bubble.style.animationDuration = (Math.random() * 5) + 5 + 's';
+        bubble.style.left = Math.random() * innerWidth + 'px';
+        document.body.append(bubble);
+    }
 }
-getNumFiles(dir)
+
+generateEmoticons();
+
+const musicIcons = document.querySelectorAll(".music-icon");
+const audio = new Audio("audio/music.mp3");
+musicIcons.forEach((icon) => {
+	icon.addEventListener("click", function () {
+		document.querySelector(".music-icon.none").classList.remove("none");
+
+		if (icon.classList.contains("fa-volume-xmark")) {
+			audio.play();
+			icon.classList.add("none");
+		} else {
+			audio.pause();
+			icon.classList.add("none");
+		}
+	});
+});
